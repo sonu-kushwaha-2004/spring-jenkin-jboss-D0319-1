4b0e77d793dc9e084c40fc71fd50215aa3142cdd
sonu-kushwaha-2004 <kushwahajsk200@gmail.com>
2022-04-05 21:30:08 +0530

    log4j2

--- src/main/java/com/jboss/spring/MyController.java
+++ src/main/java/com/jboss/spring/MyController.java
@@ -2,13 +2,26 @@ package com.jboss.spring;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.File;
+
 import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 
 
 @RestController
 public class MyController {
 	
 	private static Logger log = Logger.getLogger(MyController.class);
+	
+	static {
+		try {
+			PropertyConfigurator.configure(System.getProperty("FIRST")+File.separator+"java"+File.separator+"log4j.properties");
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 	
 	@RequestMapping("/hello")
 	public String sayHello() {
