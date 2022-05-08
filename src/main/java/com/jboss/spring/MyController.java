9ff93393538beb4c6b834efa5c1a899e8c4e542e
sonu-kushwaha-2004 <kushwahajsk200@gmail.com>
2022-05-08 14:22:24 +0530

    local log4j config

--- src/main/java/com/jboss/spring/MyController.java
+++ src/main/java/com/jboss/spring/MyController.java
@@ -12,17 +12,17 @@ import org.apache.log4j.PropertyConfigurator;
 @RestController
 public class MyController {
 	
-	 static Logger log = Logger.getLogger(MyController.class);
+	private  static  final Logger log = Logger.getLogger(MyController.class);
 	
-	static {
-		try {
-			//PropertyConfigurator.configure(System.getProperty("FIRST")+File.separator+"java"+File.separator+"log4j.properties");
-			PropertyConfigurator.configure("E:/Log/java/log4j.properties");
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+//	static {
+//		try {
+//			//PropertyConfigurator.configure(System.getProperty("FIRST")+File.separator+"java"+File.separator+"log4j.properties");
+//		//	PropertyConfigurator.configure("E:/Log/java/log4j.properties");
+//			
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//	}
 	
 	@RequestMapping("/hello")
 	public String sayHello() {
@@ -40,6 +40,7 @@ public class MyController {
 	      log.info("Hello this is an info message");  
 		
 	      String str= System.getProperty("FIRST");
+	      str="Value of FIRST from Standalone file of wildfly server : "+str;
 		return str;
 		
 	}
